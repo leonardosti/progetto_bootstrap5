8e2489b6d0febfc8e163f32c50dc1cbf09029f0a
leonardo <leonardo.osti@iisviolamarchesini.edu.it>
2024-12-10 09:07:20 -0800

    update json and js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+const getPageIndex = () => {
+    const page = window.location.pathname.split("/").pop();
+    switch (page) {
+        case "index.html":
+            return "index";
+        case "socket.html":
+            return "socket";
+        case "osi.html":
+            return "osi";
+        case "tcp-udp.html":
+            return "tcp-udp";
+        case "glossario.html":
+            return "glossario";
+        default:
+            return "index";
+    }
+};
+
+
 fetch('index.json')
     .then(respose => {
         if (!respose.ok) {
@@ -7,11 +27,17 @@ fetch('index.json')
     })
 
     .then(data => {
-        for (const id in data) {
-            const element = document.getElementById(id);
-            if (element) {
-                element.innerHTML = data[id];
+        const index = getPageIndex();
+        if (data[index]) {
+            const pageData = data[index];
+            for (const id in pageData) {
+                const element = document.getElementById(id);
+                if (element) {
+                    element.innerHTML = pageData[id];
+                }
             }
+        } else {
+            console.error(`Chiave non trovata nel file JSON.`);
         }
     })
     .catch(error => {
